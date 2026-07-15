0bbd2a0f8c117b562647bbfe7b5cf32832124d68
Angelica Garcia <70681219+YessBlack@users.noreply.github.com>
2026-07-15 13:17:11 -0500

    add counter functionality

---
+++ index.js
@@ -0,0 +1,21 @@
+const btnIncrease = document.getElementById('butttonIncrease')
+const btnDecrease = document.getElementById('butttonDecrease')
+const btnReset = document.getElementById('buttonReset')
+const idCounter = document.getElementById('counter')
+
+let counter = 0;
+
+btnIncrease.addEventListener('click', () => {
+  counter++;
+  idCounter.textContent = counter;
+});
+
+btnDecrease.addEventListener('click', () => {
+  counter--;
+  idCounter.textContent = counter;
+});
+
+btnReset.addEventListener('click', () => {
+  counter = 0;
+  idCounter.textContent = counter;
+});
